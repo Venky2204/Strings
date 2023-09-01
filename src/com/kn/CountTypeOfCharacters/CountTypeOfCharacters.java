54becb4db3193d310ae99471f6769cf63532c337
Venkatesh Pothuraju <140961144+Venky2204@users.noreply.github.com>
2023-09-01 20:33:46 +0530

    Update CountTypeOfCharacters.java

--- src/com/kn/CountTypeOfCharacters/CountTypeOfCharacters.java
+++ src/com/kn/CountTypeOfCharacters/CountTypeOfCharacters.java
@@ -7,20 +7,17 @@ public class CountTypeOfCharacters {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String s = scan.nextLine();
-		// char[] crr = s.toCharArray();
-		String s1 = s.toUpperCase();
-		int withoutSplCharacters = Count.countVowels(s1)+Count.countNumbers(s1)+Count.countConsonants(s1);
-		int countSpCharacters = s1.length() - withoutSplCharacters;
-		System.out.println("Vowels : "+Count.countVowels(s1));
-		System.out.println("Consonants : "+Count.countConsonants(s1));
-		System.out.println("Numbers : "+Count.countNumbers(s1));
-		System.out.println("SpecialCharacters : "+countSpCharacters);
+		Count c = new Count();
+		
+		System.out.println("====================================================");
+		
+//		c.countVowels(s);
+//		c.countConsonants(s);
+//		c.countNumbers(s);
+//		c.countSpaces(s);
+		c.countSpecialCharacters(s);
 		
 		scan.close();
 	}
 
-	
-
-	}
-
-
+}
